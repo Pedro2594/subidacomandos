7a676d9e122885eb0e753f6c315f712c2dfb066d
Pedro2594 <pedro.ponce1@ujcv.edu.hn>
2021-03-09 01:04:09 -0600

    subida de proyecto con comandos

---
+++ src/hn/edu/ujcv/pii/pii/Investigacion/Main.java
@@ -0,0 +1,9 @@
+package hn.edu.ujcv.pii.pii.Investigacion;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Segunda Prueba Comandos");
+    }
+}
